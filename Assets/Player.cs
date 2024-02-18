bdd51157c5246a0a890e4f05f25bf712d2164736
Omenys <105769289+Omenys@users.noreply.github.com>
2024-02-17 18:48:18 -0600

    Space bar points work

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Score myScore;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myScore = FindObjectOfType<Score>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            myScore.AddPoints(1);
+        }
     }
 }
--- Assets/Score.cs
+++ Assets/Score.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 public class Score : MonoBehaviour
 {
     TMP_Text myText;
-
     int score;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        myText = FindObjectOfType<TMP_Text>();
     }
 
     // Update is called once per frame
@@ -22,5 +21,6 @@ public class Score : MonoBehaviour
     public void AddPoints(int amount)
     {
         score += amount;
+        myText.text = score.ToString();
     }
 }
